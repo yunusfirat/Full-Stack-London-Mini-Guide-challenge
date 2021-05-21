d32379457005af54f40989cf52d446315c9c131d
yunusfirat <yunusfirat44@gmail.com>
2021-05-21 21:24:21 +0100

    trying to deploy the project on heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import hospitalRoutes from "./routes/hospitals.js";
 import collegeRoutes from "./routes/colleges.js";
 import doctorRoutes from "./routes//doctors.js";
 import singleRoutes from "./routes/single.js";
+import path from "path";
 
 const app = express();
 app.use(bodyParser.json( {limit: "30mb", extended: true }))
@@ -29,6 +30,13 @@ app.use("/", collegeRoutes);
 app.use("/doctors", doctorRoutes);
 app.use("/",doctorRoutes)
 
+if(process.env.NODE_ENV === "production"){
+    // set static folder
+    app.use(express.static("client/build"));
 
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
